dc577ec6b4ce85b523919e0e5c0d23463754395c
auui <auui@qq.com>
2024-09-08 00:34:35 +0800

    一些配置文件

--- aidada-frontend/src/request.ts
+++ aidada-frontend/src/request.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { Message } from "@arco-design/web-vue";
 
+export const isDev=process.env.NODE_ENV==='development';
+
 const myAxios = axios.create({
-  baseURL: "http://localhost:8101",
+  baseURL: isDev?"http://localhost:8101":"",
   timeout: 60000,
   withCredentials: true,
 });
